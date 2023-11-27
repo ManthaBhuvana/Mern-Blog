c845787df8d280486c887bc68d1b42b4dcaaeacf
rajesh <rajesh.t7982@gmail.com>
2023-11-27 20:23:30 +0530

    routing demo

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,20 +1,62 @@
 import "./App.css";
-import A from "./components/context-demo-1/A";
-import D from "./components/context-demo-1/D";
-
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import RootLayout from "./components/RootLayout";
+import Home from './components/home/Home';
+import Register from './components/register/Register';
+import Login from './components/login/Login';
+import Technologies from './components/technologies/Technologies';
+import ErrorRoute from "./components/ErrorRoute";
+import Java from './components/java/Java'
+import NodeJs from './components/nodejs/NodeJs'
+import Vue from './components/vue/VueJs'
 
 
 function App() {
-  //state
+  //createw browser router obj
+  let router=createBrowserRouter([
+   {
+    path:'',
+    element:<RootLayout />,
+    errorElement:<ErrorRoute />,
+    children:[
+      {
+        path:'',
+        element:<Home />
+      },
+      {
+        path:"register",
+        element:<Register />
+      },
+      {
+        path:"login",
+        element:<Login />
+      },
+      {
+        path:"technologies",
+        element:<Technologies />,
+        children:[
+          {
+            path:'java',
+            element:<Java />
+          },
+          {
+            path:"nodejs",
+            element:<NodeJs />
+          },
+          {
+            path:"vue",
+            element:<Vue />
+          }
+        ]
+      }
+    ]
+   }
+  ])
 
   return (
     <div>
-      <div className="container">
-        {/* nest StateDemo */}
-
-        <A />
-        <D />
-      </div>
+      {/* provide BrowserRouter obj to application */}
+      <RouterProvider router={router} />
     </div>
   );
 }
